6c4b5107b03581681551b5210302cd33f4b19193
kimminsic <alstlr1135@gmail.com>
2022-09-16 11:40:17 +0900

    메뉴추가

--- src/Domestic/DomMain.jsx
+++ src/Domestic/DomMain.jsx
@@ -7,7 +7,9 @@ import ScrollTop from "../components/ScrollTop";
 import DomMetaTag from "./components/DomMetaTag";
 import axios from "axios";
 import "bootstrap/js/src/collapse.js";
-import DomSlick from "./components/DomSlick";
+import DomFestivalSlick from "./components/DomFestivalSlick";
+import DomService from "./components/DomServiceMenu";
+import DomRentSlick from "./components/DomRentlSlick";
 const DomMain = () => {
   return (
     <div>
@@ -85,7 +87,7 @@ const DomMain = () => {
         }}
       >
         <div className="container text-center py-5">
-          <h1 className="text-white mb-4">국내</h1>
+          <h1 className="text-white mb-4 fs-1">국내</h1>
 
           <div className="mx-auto">
             <div className="input-group">
@@ -102,79 +104,9 @@ const DomMain = () => {
           </div>
         </div>
       </div>
-      <DomSlick />
-      <div className="container-fluid pt-5">
-        <div className="container">
-          <div className="text-center pb-2">
-            <h6 className="text-primary text-uppercase font-weight-bold">
-              Our Services
-            </h6>
-            <h1 className="mb-4">Best Logistic Services</h1>
-          </div>
-          <div className="row pb-3">
-            <div className="col-lg-3 col-md-6 text-center mb-5">
-              <div className="d-flex align-items-center justify-content-center bg-primary mb-4 p-4">
-                <i className="fa fa-2x fa-plane text-dark pr-3"></i>
-                <h6 className="text-white font-weight-medium m-0">
-                  Air Freight
-                </h6>
-              </div>
-              <p>
-                Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet
-                diam sea est diam
-              </p>
-              <a className="border-bottom text-decoration-none" href="/">
-                Read More
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-6 text-center mb-5">
-              <div className="d-flex align-items-center justify-content-center bg-primary mb-4 p-4">
-                <i className="fa fa-2x fa-ship text-dark pr-3"></i>
-                <h6 className="text-white font-weight-medium m-0">
-                  Ocean Freight
-                </h6>
-              </div>
-              <p>
-                Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet
-                diam sea est diam
-              </p>
-              <a className="border-bottom text-decoration-none" href="/">
-                Read More
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-6 text-center mb-5">
-              <div className="d-flex align-items-center justify-content-center bg-primary mb-4 p-4">
-                <i className="fa fa-2x fa-truck text-dark pr-3"></i>
-                <h6 className="text-white font-weight-medium m-0">
-                  Land Transport
-                </h6>
-              </div>
-              <p>
-                Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet
-                diam sea est diam
-              </p>
-              <a className="border-bottom text-decoration-none" href="/">
-                Read More
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-6 text-center mb-5">
-              <div className="d-flex align-items-center justify-content-center bg-primary mb-4 p-4">
-                <i className="fa fa-2x fa-store text-dark pr-3"></i>
-                <h6 className="text-white font-weight-medium m-0">
-                  Cargo Storage
-                </h6>
-              </div>
-              <p>
-                Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet
-                diam sea est diam
-              </p>
-              <a className="border-bottom text-decoration-none" href="/">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <DomService />
+      <DomFestivalSlick />
+      <DomRentSlick />
 
       <div className="container-fluid pt-5">
         <div className="container">
