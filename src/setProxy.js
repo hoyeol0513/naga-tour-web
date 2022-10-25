e0b98bcb3a8cb06e93a5a2f9531e9fb07a41cf2d
kimminsic <alstlr1135@gmail.com>
2022-10-25 13:23:18 +0900

    cors

--- src/setProxy.js
+++ src/setProxy.js
@@ -3,6 +3,15 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     "/api",
+    createProxyMiddleware({
+      target: "http://nagatourweb.shop:8081",
+      changeOrigin: true,
+    })
+  );
+};
+
+module.exports = function (app) {
+  app.use(
     "/user",
     createProxyMiddleware({
       target: "http://nagatourweb.shop:8081",
