1e23006332241a3b1b4fde85407a277cc855d21c
kimminsic <alstlr1135@gmail.com>
2022-10-26 11:27:58 +0900

    test

--- src/Domestic/Login.jsx
+++ src/Domestic/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
         },
       })
       .then((res) => {
-        if (res.data !== undefined) {
+        if (res.data === undefined) {
           console.log("로그인정보", res);
           localStorage.setItem("user_id", userid);
           alert("로그인성공");
