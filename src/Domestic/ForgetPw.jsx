27fa2287055e1bc81b387bced00cf4aae865e312
kimminsic <alstlr1135@gmail.com>
2022-10-27 11:42:40 +0900

    비밀번호 재설정 기능 구현v1

--- src/Domestic/ForgetPw.jsx
+++ src/Domestic/ForgetPw.jsx
@@ -1,23 +1,67 @@
+import axios from "axios";
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "./img/logo.png";
 const ForgetPw = () => {
+  const Navi = useNavigate();
+
+  const ResetPw = async () => {
+    if (
+      document.getElementById("userId").value !== "" &&
+      document.getElementById("userName").value !== "" &&
+      document.getElementById("email").value !== "" &&
+      document.getElementById("tel").value !== ""
+    ) {
+      await axios
+        .get(`http://nagatourweb.shop:8081/api/user/getUser`, {
+          params: {
+            userid: document.getElementById("userId").value,
+            username: document.getElementById("userName").value,
+            email: document.getElementById("email").value,
+            tel: document.getElementById("tel").value,
+          },
+        })
+        .then((res) => {
+          if (res.data.length > 0) {
+            alert("임시비밀번호를 전송하였습니다");
+            ValidateEmail();
+            Navi("/");
+          } else {
+            alert("해당 유저가 존재하지 않습니다.");
+          }
+        });
+    } else {
+      alert("전송실패!\n항목을 다시확인해주세요");
+    }
+  };
+
+  //메일 전송
+  const ValidateEmail = async () => {
+    const data = await axios.get(`http://nagatourweb.shop:8081/sendPw`, {
+      params: {
+        email: document.getElementById("email").value,
+        userid: document.getElementById("userId").value,
+      },
+    });
+  };
+
   return (
     <div>
       <div className="main-container">
         <div className="main-wrap">
           <header>
             <div className="logo-wrap">
-              <a href="http://nagatourweb.shop">
+              <Link to="/">
                 <img src={Logo} alt="logo" />
-              </a>
-              <div className="text-primary">비밀번호 찾기</div>
+              </Link>
+              <div className="text-primary">비밀번호 재설정</div>
             </div>
           </header>
           <section className="login-input-section-wrap">
             <div className="mb-4 ">
               <span className="fw-bold d-block mb-1">아이디</span>
               <div className="login-input-wrap d-flex">
-                <input id="id" type="text" maxlength="20" />
+                <input id="userId" type="text" maxlength="20" />
               </div>
 
               <span
@@ -45,23 +89,10 @@ const ForgetPw = () => {
             <div className="mb-4">
               <div className="d-flex">
                 <span className="fw-bold d-block mb-1">이메일</span>
-
-                <span
-                  className="ml-2 mt-1"
-                  style={{ color: "red", fontSize: "12px" }}
-                ></span>
-
-                <span
-                  className=" ml-2 mt-1"
-                  style={{ color: "green", fontSize: "12px" }}
-                ></span>
               </div>
               <div>
                 <div className="login-input-wrap d-flex">
-                  <input id="userID" type="text" maxlength="20"></input>
-                  <button className="btn btn-primary btn-block w-50 px-1 ">
-                    인증번호 받기
-                  </button>
+                  <input id="email" type="text" maxlength="20"></input>
                 </div>
               </div>
 
@@ -75,7 +106,7 @@ const ForgetPw = () => {
               <span className="mb-2 fw-bold d-block mb-2">휴대전화</span>
               <div className="login-input-wrap ">
                 <input
-                  id="PhoneNumber"
+                  id="tel"
                   className="d-flex"
                   type="text"
                   maxlength="20"
@@ -93,8 +124,8 @@ const ForgetPw = () => {
               </div>
             </div>
 
-            <div className="login-button-red mb-5">
-              <button>비밀번호 찾기</button>
+            <div className="login-button-wrap mb-5">
+              <button onClick={ResetPw}>비밀번호 찾기</button>
             </div>
           </section>
         </div>
